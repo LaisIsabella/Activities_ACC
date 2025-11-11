9cee6b90a54afa5b58d1065a80e616261ac21743
ppbreno <brenonalin@hotmail.com>
2025-11-11 16:44:03 -0300

    feat: user register and authenticate interface

--- Codigo/src/main/java/com/mycompany/projeto_acc/Projeto_acc.java
+++ Codigo/src/main/java/com/mycompany/projeto_acc/Projeto_acc.java
@@ -1,16 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.projeto_acc;
 
-/**
- *
- * @author Carol
- */
+import catalog.CoordinatorCatalog;
+import catalog.SupervisorCatalog;
+import catalog.StudentCatalog;
+import controller.CoordinatorController;
+import controller.SupervisorController;
+import controller.StudentController;
+import view.MainMenuView;
+
 public class Projeto_acc {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Instancia os catálogos (armazenamento em memória)
+        CoordinatorCatalog coordinatorCatalog = new CoordinatorCatalog();
+        SupervisorCatalog supervisorCatalog = new SupervisorCatalog();
+        StudentCatalog studentCatalog = new StudentCatalog();
+
+        // Cria os controladores com seus catálogos
+        CoordinatorController coordinatorController = new CoordinatorController(coordinatorCatalog);
+        SupervisorController supervisorController = new SupervisorController(supervisorCatalog);
+        StudentController studentController = new StudentController(studentCatalog);
+
+        // Abre o menu principal (interface gráfica)
+        new MainMenuView(
+            coordinatorController,
+            supervisorController,
+            studentController
+        );
     }
 }
